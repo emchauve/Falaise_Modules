4c27d8dcafcaac93f1cc7721cc3507437c8b6bf9
emchauve <chauveau@cenbg.in2p3.fr>
2021-05-05 23:36:59 +0200

    upgrade with energy correction retrieval

--- FLMANU/FLMANU_CD.cc
+++ FLMANU/FLMANU_CD.cc
@@ -14,7 +14,7 @@
 #include <vector>
 #include <array>
 
-uint32_t geomid_to_omnum(const geomtools::geom_id &geomid)
+uint32_t geomid_to_omnum (const geomtools::geom_id &geomid)
 {
   switch (geomid.get_type()) {
   case 1302: // mwall
@@ -68,20 +68,36 @@ private:
   //   vwall_it_flag=0x10,
   //   vwall_fr_flag=0x20};
 
+  unsigned long long event_id;
+
   // std::vector<unsigned short>   flag;
   std::vector<unsigned short> om_num;
 
-  std::vector<float>          energy;
-  std::vector<float>    sigma_energy;
-
-  std::vector<float>            time;
-  std::vector<float>      sigma_time;
+  std::vector<float> energy;
+  std::vector<float> energy_u;
+  std::vector<float> energy_bc;
+  std::vector<float> energy_bcu;
+  std::vector<float> sigma_energy;
+  std::vector<float> sigma_energy_u;
+  std::vector<float> sigma_energy_bc;
+  std::vector<float> sigma_energy_bcu;
+  std::vector<float> time;
+  std::vector<float> sigma_time;
   
-  std::vector<float>     energy_true;
-  std::vector<float>       time_true;
-  std::vector<short>       nhit_true;
+  std::vector<float> energy_true;
+  std::vector<float> energy_u_true;
+  std::vector<float> energy_bc_true;
+  std::vector<float> energy_bcu_true;
+  std::vector<float> time_true;
+  std::vector<short> nhit_true;
  
+  bool mw_only;
+  bool mw8_only;
+
+  float energy_cut;
+
   std::string output_filename;
+
   TTree *output_tree;
 };
 
@@ -104,10 +120,20 @@ void FLMANU_CD::initialize (const datatools::properties & myConfig, datatools::s
 {
   // std::cout << "FLMANU_CD::initialize()" << std::endl;
   
+  if (myConfig.has_key("mw_only"))
+    mw_only = myConfig.fetch_boolean("mw_only");
+  else mw_only = false;
+
+  if (myConfig.has_key("mw8_only"))
+    mw8_only = myConfig.fetch_boolean("mw8_only");
+  else mw8_only = false;
+
+  if (myConfig.has_key("energy_cut"))
+    energy_cut = myConfig.fetch_real("energy_cut");
+  else energy_cut = 0;
+
   if (myConfig.has_key("output_filename"))
-    {
-      output_filename = myConfig.fetch_string("output_filename");
-    }
+    output_filename = myConfig.fetch_string("output_filename");
 
   ////////////////////////////////
   
@@ -122,17 +148,30 @@ void FLMANU_CD::initialize (const datatools::properties & myConfig, datatools::s
   output_tree = new TTree ("output", "");
   output_tree->SetDirectory(0);
 
+  output_tree->Branch("event_id", &event_id);
+
   // output_tree->Branch("flag",                  &flag);
-  output_tree->Branch("om_num",              &om_num);
+  output_tree->Branch("om_num", &om_num);
 
-  output_tree->Branch("energy",              &energy);
-  output_tree->Branch("sigma_energy",  &sigma_energy);
+  output_tree->Branch("energy", &energy);
+  output_tree->Branch("energy_u", &energy_u);
+  output_tree->Branch("energy_bc", &energy_bc);
+  output_tree->Branch("energy_bcu", &energy_bcu);
+
+  // output_tree->Branch("sigma_energy", &sigma_energy);
+  // output_tree->Branch("sigma_energy_u", &sigma_energy_u);
+  // output_tree->Branch("sigma_energy_bc", &sigma_energy_bc);
+  // output_tree->Branch("sigma_energy_bcu", &sigma_energy_bcu);
+
   output_tree->Branch("time",                  &time);
-  output_tree->Branch("sigma_time",      &sigma_time);
+  // output_tree->Branch("sigma_time",      &sigma_time);
 
-  output_tree->Branch("energy_true",         &energy_true);
-  output_tree->Branch("time_true",             &time_true);
-  output_tree->Branch("nhit_true",             &nhit_true);
+  output_tree->Branch("energy_true",  &energy_true);
+  output_tree->Branch("energy_u_true",  &energy_u_true);
+  output_tree->Branch("energy_bc_true",  &energy_bc_true);
+  output_tree->Branch("energy_bcu_true",  &energy_bcu_true);
+  output_tree->Branch("time_true",      &time_true);
+  output_tree->Branch("nhit_true",      &nhit_true);
 
   this->_set_initialized(true);
 }
@@ -144,16 +183,23 @@ dpp::chain_module::process_status FLMANU_CD::process(datatools::things &event)
   int nhit_sd = 0;
   int nhit_cd = 0;
 
-  ++total_entries;
+  event_id = total_entries++;
 
   ////////
   // SD //
   ////////
 
   float tmp_energy_true[712];
+  float tmp_energy_u_true[712];
+  float tmp_energy_bc_true[712];
+  float tmp_energy_bcu_true[712];
   float tmp_time_true[712];
   short tmp_nhit_true[712];
+
   memset(tmp_energy_true, 0, 712*sizeof(float));
+  memset(tmp_energy_u_true, 0, 712*sizeof(float));
+  memset(tmp_energy_bc_true, 0, 712*sizeof(float));
+  memset(tmp_energy_bcu_true, 0, 712*sizeof(float));
   memset(tmp_time_true, 0, 712*sizeof(float));
   memset(tmp_nhit_true, 0, 712*sizeof(short));
 
@@ -161,9 +207,12 @@ dpp::chain_module::process_status FLMANU_CD::process(datatools::things &event)
 
   std::vector<std::string> hit_categories;
   hit_categories.push_back("calo");
-  hit_categories.push_back("xcalo");
-  hit_categories.push_back("gveto");
-
+  if (!(mw_only || mw8_only))
+    {
+      hit_categories.push_back("xcalo");
+      hit_categories.push_back("gveto");
+    }
+  
   for (std::string hit_category : hit_categories)
     {
       if (!SD.has_step_hits(hit_category))
@@ -172,13 +221,14 @@ dpp::chain_module::process_status FLMANU_CD::process(datatools::things &event)
       for (auto & a_step_hit : SD.get_step_hits(hit_category))
 	{
 	  short an_om_num = geomid_to_omnum(a_step_hit->get_geom_id());
-	  
+
 	  if (an_om_num >= 712) {
 	    printf("*** om_num (sd) = %d\n", an_om_num);
 	    continue;}
 
 	  if (tmp_energy_true[an_om_num] == 0) // first hit in this OM
 	    tmp_time_true[an_om_num] = a_step_hit->get_time_start();
+
 	  else if (a_step_hit->get_time_start() < tmp_time_true[an_om_num])
 	    tmp_time_true[an_om_num] = a_step_hit->get_time_start();
 	  
@@ -199,10 +249,17 @@ dpp::chain_module::process_status FLMANU_CD::process(datatools::things &event)
   ////////
 
   float tmp_energy[712];
+  float tmp_energy_u[712];
+  float tmp_energy_bc[712];
+  float tmp_energy_bcu[712];
   float tmp_sigma_energy[712];
   float tmp_time[712];
   float tmp_sigma_time[712];
-  memset(tmp_energy,       0, 712*sizeof(float));
+
+  memset(tmp_energy, 0, 712*sizeof(float));
+  memset(tmp_energy_u, 0, 712*sizeof(float));
+  memset(tmp_energy_bc, 0, 712*sizeof(float));
+  memset(tmp_energy_bcu, 0, 712*sizeof(float));
   memset(tmp_sigma_energy, 0, 712*sizeof(float));
   memset(tmp_time,         0, 712*sizeof(float));
   memset(tmp_sigma_time,   0, 712*sizeof(float));
@@ -220,6 +277,23 @@ dpp::chain_module::process_status FLMANU_CD::process(datatools::things &event)
       tmp_energy[an_om_num] = calo_hit->get_energy();
       tmp_sigma_energy[an_om_num] = calo_hit->get_sigma_energy();
 
+      // retrieve energy detail in auxilliaries
+
+      const datatools::properties& calo_hit_aux = calo_hit->get_auxiliaries();
+
+      if (calo_hit_aux.has_key("edep"))
+	{
+	  // tmp_energy_true[an_om_num] = calo_hit_aux.fetch_real("edep");
+	  tmp_energy_u_true[an_om_num] = calo_hit_aux.fetch_real("edep_u");
+	  tmp_energy_bc_true[an_om_num] = calo_hit_aux.fetch_real("edep_bc");
+	  tmp_energy_bcu_true[an_om_num] = calo_hit_aux.fetch_real("edep_bcu");
+
+	  // tmp_energy[an_om_num] = calo_hit_aux.fetch_real("evis");
+	  tmp_energy_u[an_om_num] = calo_hit_aux.fetch_real("evis_u");
+	  tmp_energy_bc[an_om_num] = calo_hit_aux.fetch_real("evis_bc");
+	  tmp_energy_bcu[an_om_num] = calo_hit_aux.fetch_real("evis_bcu");
+	}
+
       tmp_time[an_om_num] = calo_hit->get_time();
       tmp_sigma_time[an_om_num] = calo_hit->get_sigma_time();
     }
@@ -227,17 +301,25 @@ dpp::chain_module::process_status FLMANU_CD::process(datatools::things &event)
   for (int om=0; om<712; om++)
     if (tmp_energy[om] > 0) nhit_cd++;
   
-  //
-
   // flag.clear();
   om_num.clear();
 
   energy.clear();
+  energy_u.clear();
+  energy_bc.clear();
+  energy_bcu.clear();
   sigma_energy.clear();
+  sigma_energy_u.clear();
+  sigma_energy_bc.clear();
+  sigma_energy_bcu.clear();
+
   time.clear();
   sigma_time.clear();
 
   energy_true.clear();
+  energy_u_true.clear();
+  energy_bc_true.clear();
+  energy_bcu_true.clear();
   time_true.clear();
   nhit_true.clear();
 
@@ -246,14 +328,26 @@ dpp::chain_module::process_status FLMANU_CD::process(datatools::things &event)
       if (tmp_energy_true[om] <= 0)
 	continue;
 
+      if (tmp_energy[om] < energy_cut)
+	continue;
+
+      if (mw8_only && ((om%13)==0 || (om%13)==12))
+	continue;
+
       om_num.push_back(om);
 
       energy.push_back(tmp_energy[om]);
+      energy_u.push_back(tmp_energy_u[om]);
+      energy_bc.push_back(tmp_energy_bc[om]);
+      energy_bcu.push_back(tmp_energy_bcu[om]);
       sigma_energy.push_back(tmp_sigma_energy[om]);
       time.push_back(tmp_time[om]);
       sigma_time.push_back(tmp_sigma_time[om]);
 
       energy_true.push_back(tmp_energy_true[om]);
+      energy_u_true.push_back(tmp_energy_u_true[om]);
+      energy_bc_true.push_back(tmp_energy_bc_true[om]);
+      energy_bcu_true.push_back(tmp_energy_bcu_true[om]);
       time_true.push_back(tmp_time_true[om]);
       nhit_true.push_back(tmp_nhit_true[om]);
     }
